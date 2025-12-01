d9eff47d8e2eff0b703a4f5e2e97cd6b96ed9b8d
radjab356 <radjab.beddiar1988@gmail.com>
2025-12-01 10:02:35 +0100

    Update farmers.py

--- src/farmers.py
+++ src/farmers.py
@@ -1,4 +1,4 @@
-# === Contribution by Member 3 – [Member 3 Name] – DevOps Project 2025 ===
+# === Contribution by Member 3 – radjab beddiar – DevOps Project 2025 ===
 # Farmers list, transfers, returns
 
 from PyQt6.QtWidgets import (
